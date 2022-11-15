ebc4a088188b961cd0cbe37069751ef4fec272d8
adruich ali <adruich.ali@gmail.com>
2022-11-15 15:16:36 +0100

    First

---
+++ app.js
@@ -0,0 +1,58 @@
+const notes = require('./notes.js')
+const yargs = require('yargs')
+
+
+// Customize Yarg Version 
+yargs.version('1.1.0')
+// Craete The Add Command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+       notes.addNote(argv.title, argv.body)
+    }
+})
+// Craete the Remove Command
+yargs.command({
+    command: 'remove',
+    describe: 'Removing The note',
+    builder:{
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+    },
+    handler: function(argv){
+        notes.removeNote(argv.title)
+    }
+})
+// Craete the List Command
+yargs.command({
+    command: 'list',
+    describe: 'Listing Out all The Notes',
+    handler: function(){
+        console.log('Listing Out all The Notes')
+    }
+})
+// Craete the Read Command
+yargs.command({
+    command: 'read',
+    describe: 'Read The note',
+    handler: function(){
+        console.log('Reading The Note!')
+    }
+})
+// Add, Remove, Read, List
+
+yargs.parse()
